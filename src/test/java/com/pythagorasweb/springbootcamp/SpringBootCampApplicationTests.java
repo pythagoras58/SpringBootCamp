a43c995bb14b0c4aa10901242158dc9292f9b7d4
pythagoras58 <solomondanso58@gmail.com>
2023-02-17 23:29:46 +0000

    Initial commit

---
+++ src/test/java/com/pythagorasweb/springbootcamp/SpringBootCampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pythagorasweb.springbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
